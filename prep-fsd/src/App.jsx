d898d8773943045e179a0eae10e35d66e216e198
Selvan-S <selvan0023@gmail.com>
2024-04-05 09:37:44 +0530

    prep-fsd files added

---
+++ prep-fsd/src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Greet />
+    </>
+  );
+}
+
+function Greet() {
+  return (
+    <div>
+      <h1>Welcome to React</h1>
+    </div>
+  );
+}
+
+export default App;
